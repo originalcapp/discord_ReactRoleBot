5c0cb2aa9b14999ffd672f66ccf0a648a4d9b341
capp <79117677+originalcapp@users.noreply.github.com>
2024-05-31 09:08:09 +0200

    Update rolebot.py
    Added comments

--- rolebot.py
+++ rolebot.py
@@ -10,8 +10,8 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-    server_id = SERVER ID GOES HERE
-    roles_channel_id = ROLE CHANNEL ID
+    server_id = SERVER ID GOES HERE     # Change "SERVER ID GOES HERE"
+    roles_channel_id = ROLE CHANNEL ID  # Change "ROLE CHANNEL ID" 
 
     guild = bot.get_guild(server_id)
     if guild:
@@ -20,17 +20,17 @@ async def on_ready():
         if roles_channel:
             message = await roles_channel.send(
                 "Please react to the roles you want.\n"
-                ":EMOJI_NAME1: - DeerIsle\n"
-                ":EMOJI_NAME2: - Nyheim\n"
-                ":EMOJI_NAME3: - Chernarus"
+                ":EMOJI_NAME1: - Role1\n"       # Change "Role and EMOJI_NAME 1 2 and 3"
+                ":EMOJI_NAME2: - Role2\n"
+                ":EMOJI_NAME3: - Role3"
             )
-            for emoji in ['EMOJI1', 'EMOJI2', 'EMOJI3']:
+            for emoji in ['EMOJI1', 'EMOJI2', 'EMOJI3']:        # Change EMOJI1, 2 and 3 to emojis
                 await message.add_reaction(emoji)
 
 @bot.command()
 async def show_roles(ctx):
-    server_id = SERVER ID GOES HERE
-    roles_channel_id = ROLE CHANNEL ID
+    server_id = SERVER ID GOES HERE     # Change "SERVER ID GOES HERE"
+    roles_channel_id = ROLE CHANNEL ID  # Change "ROLE CHANNEL ID" 
 
     if ctx.guild.id == server_id:
         roles_channel = ctx.guild.get_channel(roles_channel_id)
@@ -38,9 +38,9 @@ async def show_roles(ctx):
         if roles_channel:
             await roles_channel.send(
                 "Please react to the roles you want.\n"
-                ":EMOJI_NAME1: - DeerIsle\n"
-                ":EMOJI_NAME2: - Nyheim\n"
-                ":EMOJI_NAME3: - Chernarus"
+                ":EMOJI_NAME1: - Role1\n"       # Change "Role" and EMOJI_NAME 1 2 and 3
+                ":EMOJI_NAME2: - Role2\n"
+                ":EMOJI_NAME3: - Role3"
             )
 
 @bot.event
@@ -50,14 +50,14 @@ async def on_raw_reaction_add(payload):
     user_id = payload.user_id
     emoji = payload.emoji.name
 
-    server_id = SERVER ID GOES HERE
+    server_id = SERVER ID GOES HERE     # Change "SERVER ID GOES HERE"
     role_ids = {
-        'EMOJI1': ROLE ID,
-        'EMOJI2': ROLE ID,
-        'EMOJI3': ROLE ID,
+        'EMOJI1': ROLE ID,      # Change "ROLE ID" and EMOJI1, 2 and 3 to emojis
+        'EMOJI2': ROLE ID,      
+        'EMOJI3': ROLE ID,      
     }
 
-    if channel_id == CHANNEL ID:
+    if channel_id == CHANNEL ID:        # Change "CHANNEL ID"
         guild = bot.get_guild(server_id)
         member = await guild.fetch_member(user_id)
         
@@ -77,14 +77,14 @@ async def on_raw_reaction_remove(payload):
     user_id = payload.user_id
     emoji = payload.emoji.name
 
-    server_id = SERVER ID GOES HERE
+    server_id = SERVER ID GOES HERE     # Change "SERVER ID GOES HERE"
     role_ids = {
-        'EMOJI1': ROLE ID,
-        'EMOJI2': ROLE ID,
-        'EMOJI3': ROLE ID,
+        'EMOJI1': ROLE ID,      # Change "ROLE ID" and EMOJI1, 2 and 3 to emojis
+        'EMOJI2': ROLE ID,      
+        'EMOJI3': ROLE ID,      
     }
 
-    if channel_id == CHANNEL ID:
+    if channel_id == CHANNEL ID:        # Change "CHANNEL ID"
         guild = bot.get_guild(server_id)      
         member = await guild.fetch_member(user_id)
 
@@ -95,4 +95,4 @@ async def on_raw_reaction_remove(payload):
             print(f'Removed {role.name} from {member.name}')
 
 
-bot.run('TOKEN')
+bot.run('TOKEN')        # Change "TOKEN"
